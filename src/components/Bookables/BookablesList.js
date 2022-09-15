1ffadad023bbe811aed7d342f445ad4635ef3fe7
unknown <togenius1@gmail.com>
2022-09-15 16:29:43 +0700

    updated

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,31 +1,44 @@
+import { useState } from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+
 import bookables from '../../static.json';
 
 export default function BookablesList() {
     const group = 'Rooms';
-
     const bookablesInGroup = bookables.bookables.filter(
         (b) => b.group === group
     );
+    const [bookableIndex, setBookableIndex] = useState(1);
 
-    let bookableIndex = 1;
-
-    function changeBookable(selectedIndex) {
-        bookableIndex = selectedIndex;
-        console.log(selectedIndex);
+    function nextBookable() {
+        setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
     }
 
     return (
-        <ul className='bookables items-list-nav'>
-            {bookablesInGroup.map((b, i) => (
-                <li
-                    key={b.id}
-                    className={i === bookableIndex ? 'selected' : null}
-                >
-                    <button className='btn' onClick={() => changeBookable(i)}>
-                        {b.title}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <ul className='bookables items-list-nav'>
+                {bookablesInGroup.map((b, i) => (
+                    <li
+                        key={b.id}
+                        className={i === bookableIndex ? 'selected' : null}
+                    >
+                        <button
+                            className='btn'
+                            onClick={() => setBookableIndex(i)}
+                        >
+                            {b.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <p>
+                <button className='btn' onClick={nextBookable} autoFocus>
+                    <FaArrowRight />
+                    <span>Next</span>
+                </button>
+            </p>
+        </div>
     );
 }
+
+
