15e9eafef4400f8297f2d621ff445a97a273c9d6
unknown <togenius1@gmail.com>
2022-09-19 09:49:14 +0700

    add selector Kit/Room

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -4,18 +4,29 @@ import { FaArrowRight } from 'react-icons/fa';
 import bookables from '../../static.json';
 
 export default function BookablesList() {
-    const group = 'Rooms';
+    const [group, setGroup] = useState('Kit');
+    const [bookableIndex, setBookableIndex] = useState(0);
+
     const bookablesInGroup = bookables.bookables.filter(
         (b) => b.group === group
     );
-    const [bookableIndex, setBookableIndex] = useState(1);
+    const groups = [...new Set(bookables.bookables.map((b) => b.group))];
 
     function nextBookable() {
         setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
     }
 
+
+
     return (
         <div>
+            <select value={group} onChange={(e) => setGroup(e.target.value)}>
+                {groups.map((g) => (
+                    <option value={g} key={g}>
+                        {g}
+                    </option>
+                ))}
+            </select>
             <ul className='bookables items-list-nav'>
                 {bookablesInGroup.map((b, i) => (
                     <li
@@ -40,5 +51,3 @@ export default function BookablesList() {
         </div>
     );
 }
-
-
