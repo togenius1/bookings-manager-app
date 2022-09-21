39abfadc9d38fca0739689cff60d59fca45285c8
unknown <togenius1@gmail.com>
2022-09-21 15:42:08 +0700

    try Json server

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+
+import Spinner from '../UI/Spinner';
+
 export default function UserPicker() {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        async function getUsers() {
+            const resp = await fetch('http://localhost:3001/users');
+            const data = await resp.json();
+            setUsers(data);
+        }
+        getUsers();
+    }, []);
+
+    if (users === null) {
+        return <Spinner />;
+    }
+
     return (
         <select>
-            <option>Users</option>
+            {users.map((u) => (
+                <option key={u.id}>{u.name}</option>
+            ))}
         </select>
     );
 }
